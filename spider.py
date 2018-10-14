ab31c15879707967f18f1a4dff6d90f043abaa2e
Louis Smith <smudger06@gmail.com>
2018-10-14 14:38:25 +0100

    fix bug in gather_links method

--- spider.py
+++ spider.py
@@ -45,7 +45,7 @@ class Spider:
         html_string = ''
         try:
             response = urlopen(page_url)
-            if response.getheader('Content-Type') == 'text/html':
+            if 'text/html' in response.getheader('Content-Type'):
                 html_bytes = response.read()
                 html_string = html_bytes.decode("utf-8")
             finder = LinkFinder(Spider.base_url, page_url)
