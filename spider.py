d8652c9e5a9fc5503822e6e5c3bfbe90015fa9b0
Louis Smith <smudger06@gmail.com>
2018-10-14 11:00:35 +0100

    add functionality to spider.py

--- spider.py
+++ spider.py
@@ -23,4 +23,48 @@ class Spider:
         self.boot()
         self.crawl('First spider', Spider.base_url)
 
+    @staticmethod
+    def boot():
+        create_project_dir(Spider.project_name)
+        create_data_files(Spider.project_name, Spider.base_url)
+        Spider.queue = file_to_set(Spider.queue_file)
+        Spider.crawled = file_to_set(Spider.crawled_file)
 
+    @staticmethod
+    def crawl(thread_name, page_url):
+        if page_url not in Spider.crawled:
+            print(thread_name + ' is now crawling ' + page_url)
+            print('Queued: ' + str(len(Spider.queue)) + ' | Crawled: ' + str(len(Spider.crawled)))
+            Spider.add_links_to_queue(Spider.gather_links(page_url))
+            Spider.queue.remove(page_url)
+            Spider.crawled.add(page_url)
+            Spider.update_files()
+
+    @staticmethod
+    def gather_links(page_url):
+        html_string = ''
+        try:
+            response = urlopen(page_url)
+            if response.getheader('Content-Type') == 'text/html':
+                html_bytes = response.read()
+                html_string = html_bytes.decode("utf-8")
+            finder = LinkFinder(Spider.base_url, page_url)
+            finder.feed(html_string)
+        except:
+            print("ERROR: Cannot crawl page " + page_url)
+            return set()
+        return finder.page_links()
+
+    @staticmethod
+    def add_links_to_queue(links):
+        for link in links:
+            if link in Spider.queue or link in Spider.crawled:
+                continue
+            if Spider.domain_name not in link:
+                continue
+            Spider.queue.add(link)
+
+    @staticmethod
+    def update_files():
+        set_to_file(Spider.queue, Spider.queue_file)
+        set_to_file(Spider.crawled, Spider.crawled_file)
